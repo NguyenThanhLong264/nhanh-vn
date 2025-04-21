9d674ad69d6ce0a01c32820cafaea8897867331d
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-04-21 15:50:03 +0700

    get token

--- src/app/api/get-token-vercel/route.js
+++ src/app/api/get-token-vercel/route.js
@@ -1,19 +1,12 @@
-// app/api/get-token-vercel/route.js
 import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
 
-export const config = {
-  matcher: '/api/get-token-vercel', // You can adjust this path as necessary
-};
-
 export async function GET() {
-  try {
-    // Fetch data from Vercel Edge Config
-    const configData = await get('mapping'); // Or another key like 'token'
-
-    return NextResponse.json(configData); // Send the mapping or token data as response
-  } catch (error) {
-    console.error('Error fetching config data:', error);
-    return NextResponse.json({ error: 'Failed to fetch config data' }, { status: 500 });
-  }
+    try {
+        const token = await get('token'); // get 'token' from Edge Config
+        return NextResponse.json({ token });
+    } catch (error) {
+        console.error('Error fetching config data:', error);
+        return NextResponse.json({ error: 'Failed to fetch token' }, { status: 500 });
+    }
 }
