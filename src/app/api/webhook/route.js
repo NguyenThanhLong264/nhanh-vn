8010e4df2068cb3e902f12da1ee0294d7d23acf0
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-04-25 10:22:03 +0700

    logic to response webhook imediately with status 200

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,18 +1,31 @@
 import { NextResponse } from 'next/server';
 import { webhookDispatcher } from '../../lib/handlers/webhookDispatcher';
+import condition from '../../data/condition.json'
 
 export async function POST(request) {
+    const token = condition.token
     try {
         const body = await request.json();
         const event = body.event;
+        const webhooksVerifyToken = body.webhooksVerifyToken;
+
 
         if (!event) {
+            console.log('Missing event type');
             return NextResponse.json({ message: 'Missing event type' }, { status: 400 });
+        } else if (!webhooksVerifyToken) {
+            console.log('Missing webhooksVerifyToken');
+            return NextResponse.json({ message: 'Missing webhooksVerifyToken' }, { status: 400 });
+        } else if (webhooksVerifyToken !== token.NhanhVN_VerifyToken) {
+            console.log('WebhooksVerifyToken not match.Received:', webhooksVerifyToken, 'Expected:', token.NhanhVN_VerifyToken);
+            return NextResponse.json({ message: 'Missing webhooksVerifyToken' }, { status: 400 });
         }
 
-        const result = await webhookDispatcher(event, body);
+        webhookDispatcher(event, body).catch(err => {
+            console.error('Async webhook handling error:', err.message);
+        });
 
-        return NextResponse.json(result.data, { status: result.status });
+        return NextResponse.json({ message: 'Received successfully' }, { status: 200 });
     } catch (error) {
         console.error('Webhook - Error:', error.message);
         return NextResponse.json(
--- src/app/lib/handlers/webhookDispatcher.js
+++ src/app/lib/handlers/webhookDispatcher.js
@@ -4,13 +4,16 @@ import { handleOrderUpdate } from './orderUpdateHandler.js';
 export async function webhookDispatcher(event, body) {
     switch (event) {
         case 'orderAdd':
+            console.log('Handler Event:', event);
             return await handleOrderAdd(body);
 
         case 'orderUpdate':
+            console.log('Handler Event:', event);
             return await handleOrderUpdate(body);
 
         case 'webhooksEnabled':
-            console.log('Webhook enabled, registered events:', body.data?.registeredEvents);
+            console.log('Handler Event:', event);
+            console.log('Webhook enabled, body.data:', body.data);
             return {
                 status: 200,
                 data: { message: 'Webhook enabled received' },
